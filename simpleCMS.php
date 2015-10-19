6ea3ae7dfc2758e7213b60064ff9644968a025f1
MK <michal@toke.com>
2015-10-19 11:53:30 +0200

    Build the Database

--- simpleCMS.php
+++ simpleCMS.php
@@ -23,7 +23,15 @@ class simpleCMS {
   }
 
   private function buildDB() {
-    
+    $sql = <<<MySQL_QUERY
+        CREATE TABLE IF NOT EXISTS testDB (
+            title	VARCHAR(150),
+            bodytext	TEXT,
+            created	VARCHAR(100)
+    )
+    MySQL_QUERY;
+
+    return mysql_query($sql);
   }
 }
 
